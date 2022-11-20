0c624a0c157fd4a74323c83dc008f8bb1d1a7d63
Akhila Sulgante <asulgante39@gmail.com>
2022-11-20 13:23:37 -0800

    Current user issue fix

--- frontend/src/components/Userinfo/Userinfo.js
+++ frontend/src/components/Userinfo/Userinfo.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Userform from "../Userform/Userform";
-import { useNavigate } from "react-router-dom";
-import "./Userinfo.css";
-export default function Userinfo() {
-  const [user, setUser] = useState({});
 
+import "./Userinfo.css";
+export default async function Userinfo() {
+  const [user, setUser] = useState({});
+  let res = await fetch("/users/getCurrentUser");
+  let resuser = await res.json();
+  setUser(resuser?.user);
+  console.log("users in userinfo", resuser);
   return (
     <div>
       <Navbar />
