0aec65792df8317bff9095a1c5bcca22e54f29c1
Zhiyi Jin <victoryjzy@gmail.com>
2022-11-21 10:11:22 -0800

    add navbar in OrderNow page

--- frontend/src/pages/OrderNow.js
+++ frontend/src/pages/OrderNow.js
@@ -1,9 +1,19 @@
 // Zhiyi Jin
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import landbg from "../images/landbg.jpg";
 
 export default function OrderNow() {
   return (
     <div>
+      <Navbar />
+      <img
+        style={{
+          width: "100%",
+        }}
+        src={landbg}
+        alt="food delivery"
+      />
       <h1>Thank you for your trust.</h1>
       <h2>
         Your oder will be delivered in 15 minutes. Please keep your phone
