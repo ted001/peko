2f7b47f596fa8dc41c066b24eee079096a8820e7
Akhila Sulgante <asulgante39@gmail.com>
2022-12-11 16:56:04 -0800

    remove unused files

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -17,10 +17,12 @@ export default function Footer(props) {
 
   let onHandleOrder = () => {
     if (!address || !phone) {
-      let r = window.confirm("Please input your address and phone number first, Thank you!");
+      let r = window.confirm(
+        "Please input your address and phone number first, Thank you!"
+      );
       if (r === true) {
         navigate("/Userhub");
-      } 
+      }
     } else {
       console.log("show dialog");
       const myDialog = new Modal("#dialog");
@@ -32,7 +34,7 @@ export default function Footer(props) {
     async function getCurrentUser() {
       let res = await fetch("/users/fetchUpdatedUser");
       let userInfo = await res.json();
-      
+
       let address = userInfo.user?.Address;
       let phone = userInfo.user?.Phoneno;
 
@@ -49,7 +51,8 @@ export default function Footer(props) {
         type="button"
         data-bs-toggle="modal"
         onClick={onHandleOrder}
-        disabled={checkedDishes.length === 0 ? true : false}>
+        disabled={checkedDishes.length === 0 ? true : false}
+      >
         Order Now
       </button>
 
@@ -58,18 +61,20 @@ export default function Footer(props) {
         id="dialog"
         tabIndex="-1"
         aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
+        aria-hidden="true"
+      >
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
               <h2 className="modal-title fs-2" id="exampleModalLabel">
-                Peco
+                Peko
               </h2>
               <button
                 type="button"
                 className="btn-close"
                 data-bs-dismiss="modal"
-                aria-label="Close"></button>
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
               <p className="fs-4">Do you decide to order these dishes?</p>
@@ -101,14 +106,16 @@ export default function Footer(props) {
               <button
                 type="button"
                 className="btn btn-secondary"
-                data-bs-dismiss="modal">
+                data-bs-dismiss="modal"
+              >
                 Cancel
               </button>
               <button
                 type="button"
                 className="btn btn-primary"
                 data-bs-dismiss="modal"
-                onClick={onOrderNowHandler}>
+                onClick={onOrderNowHandler}
+              >
                 Confirm
               </button>
             </div>
