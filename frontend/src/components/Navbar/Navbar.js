7b15ca5fe0e8d5c6df1dc01a650b80d19ea3eecb
Akhila Sulgante <asulgante39@gmail.com>
2022-12-11 16:22:47 -0800

    navbar user profile

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 import Signout from "../Signout/Signout";
 import Currentuser from "../Currentuser/Currentuser";
 import { useNavigate, Link } from "react-router-dom";
-
+import profile_icon from "../../images/profile_icon.png";
 /**
  * This function is used to build navbar to navigate around the site
  */
@@ -25,40 +25,43 @@ export default function Navbar({ render }) {
   }, [render]);
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light gradient-custom">
-        <div className="leftcontainer">
-          <h1>
-            <Link className="navbar-brand" to="/meals">
-              Peko
-            </Link>
-          </h1>
-        </div>
-        <div className="rightContainer">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
+    <nav className="navbar navbar-expand-md navbar-light gradient-custom">
+      <div className="leftcontainer">
+        <h1 style={{ fontSize: "20px" }}>
+          <Link className="navbar-brand" to="/meals">
+            Peko
+          </Link>
+        </h1>
+      </div>
+      <div className="rightContainer">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <img
+                width="20px"
+                height="20px"
+                src={profile_icon}
+                alt="profile icon"
+              />
+            </li>
+            <li className="nav-item">
+              <button
+                className="usebutton"
+                onClick={() => {
+                  navigate("/userhub");
+                }}
+              >
                 <Currentuser user={user} />
-              </li>
-              <li className="nav-item">
-                <button
-                  className="usebutton"
-                  onClick={() => {
-                    navigate("/userhub");
-                  }}
-                >
-                  Userhub
-                </button>
-              </li>
+              </button>
+            </li>
 
-              <li className="nav-item">
-                <Signout />
-              </li>
-            </ul>
-          </div>
+            <li className="nav-item">
+              <Signout />
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
--- frontend/src/components/foot/Mainfooter.js
+++ frontend/src/components/foot/Mainfooter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function mainfooter() {
   return (
-    <div>
+    <div style={{ backgroundColor: "#f5c65e", paddingtop: "20px" }}>
       <p>For enquiries please call: 1234567890 or email here:peko@gmail.com</p>
       <p>© 2022 Peko Copyright: Akhila and Jin</p>
     </div>
